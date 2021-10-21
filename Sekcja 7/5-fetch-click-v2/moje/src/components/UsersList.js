c68d10a6821f6716a59f5e4fee1c82fb6fa875b0
mateuszgorecki <5419@student.mup.edu.pl>
2021-10-21 13:12:00 +0200

    add photo to UsersList.js and create UsersList.css

--- Sekcja 7/5-fetch-click-v2/moje/src/components/UsersList.js
+++ Sekcja 7/5-fetch-click-v2/moje/src/components/UsersList.js
@@ -1,8 +1,11 @@
 import React from "react";
+import "../styles/UsersList.css";
+
 const UsersList = (props) => {
   console.log(props.users);
   const users = props.users.map((user) => (
     <li key={user.login.uuid}>
+      <img src={user.picture.large} alt={user.name.last} />
       <p>
         <strong>{`${user.name.title}  ${user.name.last}`}</strong>
       </p>
