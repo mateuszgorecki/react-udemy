fda6b276d61d0feb13f503177a38fd6ce4482cb5
mateuszgorecki <5419@student.mup.edu.pl>
2021-10-26 12:30:56 +0200

    done first version app

--- Sekcja 7/PogadaApp-v1/moje/src/components/App.js
+++ Sekcja 7/PogadaApp-v1/moje/src/components/App.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import Form from "./From";
 import Result from "./Result";
 import "../styles/App.css";
-
+const APIKey = "1141c3e1aa8845e58feea65ee43f2a23";
+const addParams = "&units=metric&lang=pl";
 class App extends Component {
   state = {
     value: "",
@@ -21,18 +22,39 @@ class App extends Component {
       value: e.target.value,
     });
   };
-
   citySubmit = (e) => {
     e.preventDefault();
-    console.log("dziala");
-    const API = `https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&units=metric&appid=1141c3e1aa8845e58feea65ee43f2a23`;
+    const API = `https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}${addParams}&appid=${APIKey}`;
 
     fetch(API)
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
+      .then((res) => {
+        if (res.ok) return res;
+        throw Error("Something isnt yes");
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        const time = new Date().toLocaleString();
+        this.setState({
+          err: false,
+          date: time,
+          sunrise: data.sys.sunrise,
+          sunset: data.sys.sunset,
+          temp: data.main.temp,
+          pressure: data.main.pressure,
+          wind: data.wind.speed,
+          city: data.name,
+        });
+      })
+      .catch(() => {
+        this.setState((prevState) => ({
+          err: true,
+          city: prevState.value,
+        }));
+      });
   };
 
   render() {
+    const { city, err } = this.state;
     return (
       <div className="main-wrapper">
         <h1>Apka pogodowa</h1>
@@ -41,7 +63,11 @@ class App extends Component {
           action={this.changeInput}
           submit={this.citySubmit}
         />
-        <Result />
+        {err ? (
+          `Nie ma w bazie miejscowości ${city}`
+        ) : city ? (
+          <Result fetch={this.fetchData} weather={this.state} />
+        ) : null}
       </div>
     );
   }
