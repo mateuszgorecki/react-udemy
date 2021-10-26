7fb63fcc020e0ecf29567c7dfe20a6e09ff2b3d4
mateuszgorecki <5419@student.mup.edu.pl>
2021-10-26 10:11:52 +0200

    dont remember what changes wasA

--- Sekcja 7/PogadaApp-v1/moje/src/components/App.js
+++ Sekcja 7/PogadaApp-v1/moje/src/components/App.js
@@ -6,6 +6,14 @@ import "../styles/App.css";
 class App extends Component {
   state = {
     value: "",
+    date: "",
+    city: "",
+    sunrise: "",
+    sunset: "",
+    temp: "",
+    pressure: "",
+    wind: "",
+    err: "",
   };
 
   changeInput = (e) => {
@@ -14,11 +22,25 @@ class App extends Component {
     });
   };
 
+  citySubmit = (e) => {
+    e.preventDefault();
+    console.log("dziala");
+    const API = `https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&units=metric&appid=1141c3e1aa8845e58feea65ee43f2a23`;
+
+    fetch(API)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
   render() {
     return (
       <div className="main-wrapper">
         <h1>Apka pogodowa</h1>
-        <Form value={this.state.value} action={this.changeInput} />
+        <Form
+          value={this.state.value}
+          action={this.changeInput}
+          submit={this.citySubmit}
+        />
         <Result />
       </div>
     );
--- Sekcja 7/PogadaApp-v1/moje/src/components/From.js
+++ Sekcja 7/PogadaApp-v1/moje/src/components/From.js
@@ -3,7 +3,7 @@ import React from "react";
 const Form = (props) => {
   return (
     <div>
-      <form>
+      <form onSubmit={props.submit}>
         <input
           type="text"
           value={props.value}
