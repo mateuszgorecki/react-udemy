3831ec0a7f6e734b3b111ee66acbda066bf0c3a6
Mateusz <mateuszgorecki770@gmail.com>
2021-10-27 12:07:26 +0200

    finish application

--- Sekcja 8/FormValidation/moje/src/App.js
+++ Sekcja 8/FormValidation/moje/src/App.js
@@ -7,21 +7,120 @@ class App extends Component {
     email: "",
     password: "",
     checkbox: false,
+
+    errors: {
+      username: false,
+      email: false,
+      password: false,
+      checkbox: false,
+    },
+  };
+
+  messages = {
+    username_incorrect:
+      "Nazwa musi być dłuższa niż 10 znaków i nie może zawierać spacji",
+    email_incorrect: "Email musi zawierać @",
+    password_incorrect: "Hasło musi mieć 8 znaków",
+    checkbox_incorrect: "Regulamin musi być zaakceptowany",
   };
 
   userChange = (e) => {
     // console.log(e.target.value);
-    // console.log(e.target.type);
+    const type = e.target.type;
+    const value = e.target.value;
     const name = e.target.name;
-    this.setState({
-      [name]: e.target.value,
-    });
+    const checked = e.target.checked;
+    if (type === "text" || type === "email" || type === "password") {
+      this.setState({
+        [name]: value,
+      });
+    } else if (type === "checkbox") {
+      this.setState({
+        [name]: checked,
+      });
+    }
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log("dziala");
+
+    const validation = this.formValidation();
+    if (validation.correct) {
+      this.setState({
+        username: "",
+        email: "",
+        password: "",
+        checkbox: false,
+        message: "Formularz został wysłany",
+
+        errors: {
+          username: false,
+          email: false,
+          password: false,
+          checkbox: false,
+        },
+      });
+    } else {
+      this.setState({
+        errors: {
+          username: !validation.username,
+          email: !validation.email,
+          password: !validation.password,
+          checkbox: !validation.checkbox,
+        },
+      });
+    }
   };
+
+  formValidation = () => {
+    let username = false;
+    let email = false;
+    let password = false;
+    let checkbox = false;
+    let correct = false;
+
+    if (
+      this.state.username.length >= 10 &&
+      this.state.username.indexOf(" ") === -1
+    ) {
+      username = true;
+    }
+
+    if (this.state.email.indexOf("@") !== -1) {
+      email = true;
+    }
+
+    if (this.state.password.length === 8) {
+      password = true;
+    }
+
+    if (this.state.checkbox) {
+      checkbox = true;
+    }
+
+    if (username && email && password & checkbox) {
+      correct = true;
+    }
+
+    return {
+      correct,
+      username,
+      email,
+      password,
+      checkbox,
+    };
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.message !== "") {
+      setTimeout(() => {
+        this.setState({
+          message: "",
+        });
+      }, 3000);
+    }
+  }
+
   render() {
     return (
       <form onSubmit={this.handleSubmit} noValidate>
@@ -36,34 +135,54 @@ class App extends Component {
             onChange={this.userChange}
           />
         </label>
+        {this.state.errors.username && (
+          <span>{this.messages.username_incorrect}</span>
+        )}
+
         <label htmlFor="email">
           Email:{" "}
           <input
             type="email"
             id="email"
             name="email"
+            value={this.state.email}
             onChange={this.userChange}
           />
         </label>
+        {this.state.errors.email && (
+          <span>{this.messages.email_incorrect}</span>
+        )}
+
         <label htmlFor="password">
           Hasło:{" "}
           <input
             type="password"
             id="password"
             name="password"
+            value={this.state.password}
             onChange={this.userChange}
           />
         </label>
+        {this.state.errors.password && (
+          <span>{this.messages.password_incorrect}</span>
+        )}
+
         <label htmlFor="checkbox">
           <input
             type="checkbox"
             name="checkbox"
             id="checkbox"
+            checked={this.state.checkbox}
             onChange={this.userChange}
           />{" "}
           Akceptuje regulamin
         </label>
+        {this.state.errors.checkbox && (
+          <span>{this.messages.checkbox_incorrect}</span>
+        )}
+
         <button>Wyślij formularz </button>
+        {this.state.message && <h3>{this.state.message}</h3>}
       </form>
     );
   }
