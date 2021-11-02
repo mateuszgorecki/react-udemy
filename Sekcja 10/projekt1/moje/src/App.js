e219ea16295245aed15b825e7e89d92a63e99a03
Mateusz <mateuszgorecki770@gmail.com>
2021-11-02 11:25:00 +0100

    first steps with RR (add BrowserRouter and Link components)

--- Sekcja 10/projekt1/moje/src/App.js
+++ Sekcja 10/projekt1/moje/src/App.js
@@ -1,13 +1,30 @@
-import React, { Component } from 'react';
-import './App.css';
-
-
-
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-     <div>logo</div>
+      <BrowserRouter>
+        <div>
+          <header>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Start</Link>
+                </li>
+                <li>
+                  <Link to="/news">Aktualności</Link>
+                </li>
+                <li>
+                  <Link to="/contact">Kontakt</Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
+          <section>Strona- witaj</section>
+        </div>
+      </BrowserRouter>
     );
   }
 }
