14964bee56b12327188174f511747fda8be6084e
Mateusz <mateuszgorecki770@gmail.com>
2021-11-02 13:09:08 +0100

    create menu

--- Sekcja 10/projekt2/moje/src/layouts/Navigation.js
+++ Sekcja 10/projekt2/moje/src/layouts/Navigation.js
@@ -2,10 +2,25 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "../styles/Navigation.css";
 
+const list = [
+  { name: "start", path: "/" },
+  { name: "produkty", path: "/products" },
+  { name: "kontakt", path: "/contact" },
+  { name: "admin panel", path: "/admin" },
+];
+
 const Navigation = () => {
+  const menu = list.map((item) => (
+    <li key={item.name}>
+      <NavLink exact to={item.path}>
+        {item.name}
+      </NavLink>
+    </li>
+  ));
+
   return (
     <nav className="main">
-      <ul>menu</ul>
+      <ul>{menu}</ul>
     </nav>
   );
 };
