2c2c41154798c77afa737f7685756b11cf5f3286
Mateusz <mateuszgorecki770@gmail.com>
2021-11-03 10:20:46 +0100

    create contactpage with state and Propmt component

--- Sekcja 10/projekt2/moje/src/pages/ContactPage.js
+++ Sekcja 10/projekt2/moje/src/pages/ContactPage.js
@@ -3,11 +3,50 @@ import "../styles/ContactPage.css";
 import { Prompt } from "react-router-dom";
 
 class ContactPage extends React.Component {
+  state = {
+    value: "",
+    isEmpty: true,
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      value: "",
+      isEmpty: true,
+    });
+  };
+
+  handleChange = (e) => {
+    if (e.target.value.length > 0) {
+      this.setState({
+        value: e.target.value,
+        isEmpty: false,
+      });
+    } else {
+      this.setState({
+        value: e.target.value,
+        isEmpty: true,
+      });
+    }
+  };
+
   render() {
     return (
-      <>
-        <div>ContactPage</div>
-      </>
+      <div className="contact">
+        <form onSubmit={this.handleSubmit}>
+          <h3>Napisz do nas</h3>
+          <textarea
+            placeholder="Napisz wiadomość..."
+            value={this.state.value}
+            onChange={this.handleChange}
+          ></textarea>
+          <button>Wyślij</button>
+        </form>
+        <Prompt
+          when={!this.state.isEmpty}
+          message="Czy na pewno przechodzisz dalej?"
+        />
+      </div>
     );
   }
 }
