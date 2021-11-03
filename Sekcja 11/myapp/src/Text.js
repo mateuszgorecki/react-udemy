8e11676bf83f000fe3c731e747322372ac17b44c
Mateusz <mateuszgorecki770@gmail.com>
2021-11-03 13:43:51 +0100

    add bootstrap

---
+++ Sekcja 11/myapp/src/Text.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Text.css";
+
+const Text = () => {
+  return (
+    <>
+      <button className="btn btn-primary">przycisk</button>
+      <p className="blue">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta quo
+        veritatis doloribus sapiente sed, eveniet distinctio? Ullam dignissimos
+        omnis, earum expedita deserunt velit accusantium aut rerum eum totam
+        laboriosam alias.
+      </p>
+    </>
+  );
+};
+
+export default Text;
