adc74ec25f0046c159defbfc17be9a9a95b8b6ea
Mateusz <mateuszgorecki770@gmail.com>
2021-11-09 12:27:30 +0100

    createRef done

--- Sekcja 14/3.createref/src/App.js
+++ Sekcja 14/3.createref/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, createRef } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  textInput = createRef();
+  paragraphElement = createRef();
+
+  focusTextInput = () => this.textInput.current.focus();
+  addChar = () => (this.paragraphElement.current.textContent += "!");
+
+  render() {
+    return (
+      <div>
+        <input ref={this.textInput} type="text" />
+        <p ref={this.paragraphElement}>Hello</p>
+        <button onClick={this.focusTextInput}>Focus na input</button>
+        <button onClick={this.addChar}>Add '!'</button>
+      </div>
+    );
+  }
 }
 
 export default App;
