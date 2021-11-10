6c8540cc91cafba654b6771e7e0862aa1121026e
Mateusz <mateuszgorecki770@gmail.com>
2021-11-10 12:29:42 +0100

    finish useReducer

--- Sekcja 14/4.usereducer/moje/src/CourseInfo.jsx
+++ Sekcja 14/4.usereducer/moje/src/CourseInfo.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 
-const CourseInfo = ({id, onClickHandler, title}) => {
-  const handleOnClick = () => onClickHandler({
-    id,
-    type: 'REMOVE',
-  });
+const CourseInfo = ({ id, onClickHandler, title }) => {
+  const handleOnClick = () =>
+    onClickHandler({
+      id,
+      type: "REMOVE",
+    });
 
   return (
     <div>
       <p>{title}</p>
-      <button onClick={handleOnClick}>Usun kurs</button>
+      <button onClick={handleOnClick}>Usuń kurs</button>
     </div>
   );
 };
--- Sekcja 14/4.usereducer/moje/src/Form.jsx
+++ Sekcja 14/4.usereducer/moje/src/Form.jsx
@@ -1,24 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Form = ({addHandler}) => {
-  const [inputData, setInputData] = useState('');
+const Form = ({ addHandler }) => {
+  const [inputData, setInputData] = useState("");
 
-  const handleOnChange = event => setInputData(event.target.value);
-
+  const handleOnChange = (e) => setInputData(e.target.value);
   const handleOnClick = () => {
     const course = {
       id: Math.floor(Math.random() * 1000),
       title: inputData,
     };
 
-    addHandler({type: 'ADD', course});
-    setInputData('');
+    addHandler({ type: "ADD", course });
+    setInputData("");
   };
 
   return (
     <div>
       <input type="text" onChange={handleOnChange} value={inputData} />
-      <button onClick={handleOnClick}>Dodaj kurs</button>
+      <button onClick={handleOnClick}>Dodaj</button>
     </div>
   );
 };
