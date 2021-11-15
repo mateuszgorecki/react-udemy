37e780b56185e191ec212969ccf1cf12ebb54b8c
Mateusz <mateuszgorecki770@gmail.com>
2021-11-15 11:02:43 +0100

    get ready files to work

--- Sekcja 14/6.usecallback/src/App.js
+++ Sekcja 14/6.usecallback/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState } from "react";
+
+import Counter from "./Counter";
 
 function App() {
+  const [state, setState] = useState({ x: 0, y: 0 });
+  // const [firstCounter, setFirstCounter] = useState(0);
+  // const [secondCounter, setSecondCounter] = useState(0);
+
+  const increaseX = () => setState({ ...state, x: state.x + 1 });
+  const increaseY = () => setState({ ...state, y: state.y + 1 });
+
+  // const increaseFirstCounter = () => setFirstCounter(firstCounter + 1);
+  // const increaseSecondCounter = () => setSecondCounter(secondCounter + 1);
+
+  const firstCounterComponent = useMemo(
+    () => <Counter counter={state} index={1} />,
+    [state]
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Licznik nr1 wynosi: {state.x}</p>
+      <p>Licznik nr2 wynosi: {state.y}</p>
+      {firstCounterComponent}
+      {/* <Counter counter={state} index={1} /> */}
+      <Counter counter={state} index={2} />
+      <button onClick={increaseX}>Zwiększ licznik numer 1</button>
+      <button onClick={increaseY}>Zwiększ licznik numer 2</button>
     </div>
   );
 }
