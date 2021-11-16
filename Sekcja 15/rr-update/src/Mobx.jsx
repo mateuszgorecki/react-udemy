90207e530e8474a2f49f6a213c85d775c554d9e8
Mateusz <mateuszgorecki770@gmail.com>
2021-11-16 11:31:28 +0100

    finish useParams

--- Sekcja 15/rr-update/src/Mobx.jsx
+++ Sekcja 15/rr-update/src/Mobx.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 const MobX = () => {
+  const [inputData, setInputData] = useState("");
+  const history = useHistory();
+  const handleOnChange = (e) => setInputData(e.target.value);
+
+  const handleOnClick = () => {
+    const location = {
+      pathname: `/typescript/${inputData}`,
+    };
+
+    history.push(location);
+  };
+
   return (
     <div>
       <p>MobX</p>
+      <input onChange={handleOnChange} type="text" value={inputData} />
+      <button onClick={handleOnClick}>Sent params to TS</button>
     </div>
   );
 };
--- Sekcja 15/rr-update/src/TypeScript.jsx
+++ Sekcja 15/rr-update/src/TypeScript.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const TypeScript = () => {
+  const paramsObject = useParams();
+
+  console.log(paramsObject);
+
   return (
     <div>
       <p>TypeScript</p>
