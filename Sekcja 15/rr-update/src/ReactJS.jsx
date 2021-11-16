5e0dada7e5f77a9e40f11ab4011f8059df117cfe
Mateusz <mateuszgorecki770@gmail.com>
2021-11-16 11:15:20 +0100

    finish useHistory

--- Sekcja 15/rr-update/src/ReactJS.jsx
+++ Sekcja 15/rr-update/src/ReactJS.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 const ReactJS = () => {
+  const history = useHistory();
+
+  const handleOnClick = () => {
+    const location = {
+      pathname: "/redux",
+      state: {
+        isActive: true,
+      },
+    };
+
+    history.push(location);
+  };
+
   return (
     <div>
       <p>ReactJS</p>
+      <button onClick={handleOnClick}>Go to redux</button>
     </div>
   );
 };
--- Sekcja 15/rr-update/src/Redux.jsx
+++ Sekcja 15/rr-update/src/Redux.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 const Redux = () => {
+  const history = useHistory();
+
+  const handleOnClick = () => history.goBack();
+
   return (
     <div>
       <p>Redux</p>
+      <button onClick={handleOnClick}>Back to prev page</button>
     </div>
   );
 };
