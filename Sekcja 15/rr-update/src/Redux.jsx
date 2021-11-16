8135617d929e6dc98fe351b46ad0cc9369f19e31
Mateusz <mateuszgorecki770@gmail.com>
2021-11-16 11:21:46 +0100

    finish useLocation

--- Sekcja 15/rr-update/src/Redux.jsx
+++ Sekcja 15/rr-update/src/Redux.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 
 const Redux = () => {
   const history = useHistory();
+  const location = useLocation();
 
   const handleOnClick = () => history.goBack();
+
+  const isActive = Boolean(location.state && location.state.isActive);
 
   return (
     <div>
       <p>Redux</p>
+      <p>Informacja {String(isActive)}</p>
       <button onClick={handleOnClick}>Back to prev page</button>
     </div>
   );
